4eeb19cbcdf220e46eae4716be004a0afddcf6c9
RaikiWatanabe <k021g1324@g.neec.ac.jp>
2022-11-24 15:50:11 +0900

    プロジェクト ファイルを追加します。

---
+++ GameScene.cpp
@@ -0,0 +1,165 @@
+﻿#include "GameScene.h"
+#include <cassert>
+
+using namespace DirectX;
+
+GameScene::GameScene()
+{
+}
+
+GameScene::~GameScene()
+{
+	delete spriteBG;
+	delete particleManager;
+	delete(sprit1);
+	delete(sprit2);
+}
+
+void GameScene::Initialize(DirectXCommon* dxCommon, Input* input)
+{
+	// nullptrチェック
+	assert(dxCommon);
+	assert(input);
+
+	this->dxCommon = dxCommon;
+	this->input = input;
+
+	// デバッグテキスト用テクスチャ読み込み
+	Sprite::LoadTexture(debugTextTexNumber, L"Resources/debugfont.png");
+	// デバッグテキスト初期化
+	debugText.Initialize(debugTextTexNumber);
+
+	// テクスチャ読み込み
+	/*Sprite::LoadTexture(1, L"Resources/background.png");*/
+	/*Sprite::LoadTexture(1, L"Resources/background1.png");*/
+
+	// 背景スプライト生成
+	spriteBG = Sprite::Create(1, { 0.0f,0.0f });
+	// 3Dオブジェクト生成
+	particleManager = ParticleManager::Create();
+	particleManager->Update();
+
+	//テクスチャ２番に読み込み
+	Sprite::LoadTexture(2, L"Resources/texture.png");
+
+	//座標｛0，0｝にテクスチャ２番のスプライトを生成
+	sprit1 = Sprite::Create(2,{0, 0});
+	//座標｛500，500｝にテクスチャ２番のスプライトを生成
+	sprit2 = Sprite::Create(2, { 500,500 }, { 1,0,0,1 } ,{0,0},false,true);
+}
+
+void GameScene::Update()
+{
+	// オブジェクト移動
+	//if (input->PushKey(DIK_UP) || input->PushKey(DIK_DOWN) || input->PushKey(DIK_RIGHT) || input->PushKey(DIK_LEFT))
+	//{
+	//	// 現在の座標を取得
+	//	XMFLOAT3 position = particleManager->GetPosition();
+
+	//	// 移動後の座標を計算
+	//	if (input->PushKey(DIK_UP)) { position.y += 1.0f; }
+	//	else if (input->PushKey(DIK_DOWN)) { position.y -= 1.0f; }
+	//	if (input->PushKey(DIK_RIGHT)) { position.x += 1.0f; }
+	//	else if (input->PushKey(DIK_LEFT)) { position.x -= 1.0f; }
+
+	//	// 座標の変更を反映
+	//	particleManager->SetPosition(position);
+	//}
+
+	for (int i = 0; i < 100; i++) {
+		//X,Y,Z全て[-5.0f,+5.0f]でランダムに分布
+		const float rnd_pos = 10.0f;
+		XMFLOAT3 pos{};
+		pos.x = (float)rand() / RAND_MAX * rnd_pos - rnd_pos / 2.0f;
+		pos.y = (float)rand() / RAND_MAX * rnd_pos - rnd_pos / 2.0f;
+		pos.z = (float)rand() / RAND_MAX * rnd_pos - rnd_pos / 2.0f;
+
+		//X,Y,Z全て[-0.05f,+0.05f]でランダムに分布
+		const float rnd_vel = 0.1f;
+		XMFLOAT3 vel{};
+		vel.x = (float)rand() / RAND_MAX * rnd_vel - rnd_vel / 2.0f;
+		vel.y = (float)rand() / RAND_MAX * rnd_vel - rnd_vel / 2.0f;
+		vel.z = (float)rand() / RAND_MAX * rnd_vel - rnd_vel / 2.0f;
+		//重力に見立ててYのみ[-0.001f,0]でランダムに分布
+		const float rnd_acc = 0.001f;
+		XMFLOAT3 acc{};
+		acc.y = -(float)rand() / RAND_MAX * rnd_acc;
+
+		//追加
+		particleManager->Add(1, pos, vel, acc);
+
+	}
+
+	// カメラ移動
+	if (input->PushKey(DIK_W) || input->PushKey(DIK_S) || input->PushKey(DIK_D) || input->PushKey(DIK_A))
+	{
+		if (input->PushKey(DIK_W)) { ParticleManager::CameraMoveEyeVector({ 0.0f,+1.0f,0.0f }); }
+		else if (input->PushKey(DIK_S)) { ParticleManager::CameraMoveEyeVector({ 0.0f,-1.0f,0.0f }); }
+		if (input->PushKey(DIK_D)) { ParticleManager::CameraMoveEyeVector({ +1.0f,0.0f,0.0f }); }
+		else if (input->PushKey(DIK_A)) { ParticleManager::CameraMoveEyeVector({ -1.0f,0.0f,0.0f }); }
+	}
+
+	particleManager->Update();
+
+	if (input->PushKey(DIK_SPACE)) {
+		//現在の座標の取得
+		XMFLOAT2 position = sprit1->GetPosition();
+		//移動後の座標を計算
+		position.x += 1.0f;
+		//座標の変更を反映
+		sprit1->SetPosition(position);
+	}
+}
+
+void GameScene::Draw()
+{
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* cmdList = dxCommon->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(cmdList);
+	// 背景スプライト描画
+	spriteBG->Draw();
+	/*sprit1->Draw();
+	sprit2->Draw();*/
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	ParticleManager::PreDraw(cmdList);
+
+	// 3Dオブクジェクトの描画
+	particleManager->Draw();
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+
+	// 3Dオブジェクト描画後処理
+	ParticleManager::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(cmdList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// デバッグテキストの描画
+	debugText.DrawAll(cmdList);
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+#pragma endregion
+}
